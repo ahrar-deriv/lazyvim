deba38746dc548cd4c4e369a387714ee8af63b0b
Ahrar <ahrar@deriv.com>
2023-04-07 13:45:17 +0800

    first commit

---
+++ lua/plugins/flutter-tools.lua
@@ -0,0 +1,20 @@
+return {
+  "akinsho/flutter-tools.nvim",
+  lazy = false,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "stevearc/dressing.nvim", -- optional for vim.ui.select
+  },
+  config = function()
+    require("flutter-tools").setup({
+      widget_guides = {
+        enabled = true,
+      },
+      closing_tags = {
+        highlight = "Comment", -- highlight for the closing tag
+        prefix = "^", -- character to use for close tag e.g. > Widget
+        enabled = true, -- set to false to disable
+      },
+    })
+  end,
+}
