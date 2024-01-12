001c0d74462b5e94483baa6ba9b56faa118dc8e0
Ahrar <ahrar@deriv.com>
2024-01-12 11:17:28 +0800

    chore: lock files and init

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 if vim.g.neovide then
-  vim.o.guifont = "JetBrainsMono Nerd Font:h15"
-  vim.opt.linespace = 0
+  vim.o.guifont = "JetBrains Mono NL:h15"
+  vim.opt.linespace = 5
   vim.g.neovide_cursor_vfx_mode = "railgun"
   vim.g.neovide_input_use_logo = 1 -- enable use of the logo (cmd) key
   vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
@@ -10,6 +10,20 @@ if vim.g.neovide then
   vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
   vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
   vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+
+  -- vim.g.neovide_theme = "auto"
+
+  -- -- Helper function for transparency formatting
+  -- local alpha = function()
+  --   return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
+  -- end
+  -- -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
+  -- vim.g.neovide_transparency = 1.0
+  -- vim.g.transparency = 0.0
+  -- vim.g.neovide_background_color = "#0f1117" .. alpha()
+  -- vim.g.neovide_floating_blur_amount_x = 2.0
+  -- vim.g.neovide_floating_blur_amount_y = 2.0
+  -- vim.g.neovide_refresh_rate = 120
 end
 require("config.lazy")
 vim.opt.spell = true
